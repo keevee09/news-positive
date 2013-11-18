fae9cc4326a1e43f55ca7bf2a5a304b3297ba173
Dylan Hanning <dylanhanning123@hotmail.com>
2013-11-18 18:03:41 +0000

    Initial commit

---
+++ README.md~
@@ -0,0 +1,23 @@
+# README
+
+## About
+
+A simple Firefox addon that tries to mitigate the news' obsession with murder, killing, and murder. Works particularly well with [the BBC](www.bbc.co.uk/news).
+
+See also:
+[My other attempt at making Firefox addons](https://github.com/dmcaw221/news-vagueifier)
+
+## Inspired by / heavily adapted from
+
+..*[XKCD](http://xkcd.com/1288/)
+
+..*[Firefox cloud-to-butt addon](https://github.com/DaveRandom/cloud-to-butt-mozilla)
+
+..*[Original Chrome version by Panicsteve](https://github.com/panicsteve/cloud-to-butt)
+
+
+## To intsall
+
+Download the .xpi file and open in Firefox...
+or:
+Download from the Firefox addons page.
---
+++ data/positive.js
@@ -0,0 +1,79 @@
+
+// Credits:
+// https://github.com/panicsteve/cloud-to-butt
+// https://github.com/DaveRandom/cloud-to-butt-mozilla
+
+$(document).ready(function() {
+    var count = 0;
+    walk(document.body);
+    
+    function walk(node) 
+    {
+        // I stole this function from here:
+        // http://is.gd/mwZp7E
+    
+        var child, next;
+    
+        switch ( node.nodeType )  
+        {
+            case 1:  // Element
+               // alert("Case 1");
+            case 9:  // Document
+               // alert("Case 2");
+            case 11: // Document fragment
+                //alert("Case 11");
+                child = node.firstChild;
+                while ( child ) 
+                {
+                    next = child.nextSibling;
+                    walk(child);
+                    child = next;
+                }
+                break;
+            case 3: // Text node
+                //alert("Case 3 (text) with node content "+node.nodeValue);
+                handleText(node);
+                break;
+        }
+
+    } 
+    
+    
+    function handleText(textNode)
+    {
+        // Try using Regexp's for this:
+        //if (textNode.nodeValue == "the cloud" || textNode.nodeValue == "The Cloud" || textNode.nodeValue == "The cloud") {
+        //    count = count+1;
+       // }
+       // var rules = {"witnesses": "these dudes I know", "allegedly": "kinda probably", "very": "like totally", "researchers": "nerds"}
+        var v = textNode.nodeValue;
+        v = v.replace(/\bmurder\b/gi, "firm hugging");
+        v = v.replace(/\bmurdered\b/gi, "slightly frightened");
+        v = v.replace(/\bmurdering\b/gi, "frightening");
+        v = v.replace(/\bmurderer\b/gi, "naughty prankster");
+        v = v.replace(/\brape\b/gi, "thorough complimenting");
+        v = v.replace(/\bfire\b/gi, "candy explosion");
+        v = v.replace(/\bbeating\b/gi, "playful rough-housing");
+        v = v.replace(/\bprotest\b/gi, "singing circle");
+        v = v.replace(/\bprotests\b/gi, "singing circles");
+        v = v.replace(/\bkilled\b/gi, "given a fright");
+        v = v.replace(/\battack\b/gi, "prank");
+        v = v.replace(/\battacks\b/gi, "pranks");
+        v = v.replace(/\bgun\b/gi, "water balloon");
+        v = v.replace(/\bguns\b/gi, "water balloons");
+        v = v.replace(/\bdrugs\b/gi, "cake");
+        v = v.replace(/\bgunman\b/gi, "prankster");
+        v = v.replace(/\bgunmen\b/gi, "pranksters");
+        v = v.replace(/\binjured\b/gi, "mildly surprised");
+        v = v.replace(/\bblood\b/gi, "cranberry juice");
+        v = v.replace(/\bappalling\b/gi, "not so nice");
+        v = v.replace(/\bappalled\b/gi, "disappointed");
+        v = v.replace(/\bdisgusting\b/gi, "gross");
+        v = v.replace(/\bdisgusted\b/gi, "grossed out");
+
+        textNode.nodeValue = v;
+    
+    }
+
+//alert("final count = "+count);
+});
